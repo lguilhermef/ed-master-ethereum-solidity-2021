5e49e010b40f775de0b41e7e0b3aa3b917a87d29
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2021-08-12 15:04:03 +0100

    Lottery contract started

---
+++ contracts/lottery.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Lottery {
+    address payable[] public players;
+    address public manager;
+    
+    constructor(){
+        manager = msg.sender;
+    }
+ 
+    receive() external payable {
+        players.push(payable(msg.sender));
+    }   
+    
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+}
