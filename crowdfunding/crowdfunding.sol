ef3ccf8a29ab98d928491741c95bd539a67fdb5b
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2021-08-19 10:53:58 +0100

    Add spendingRequest function

--- crowdfunding/crowdfunding.sol
+++ crowdfunding/crowdfunding.sol
@@ -4,6 +4,17 @@ pragma solidity >0.6.0 <0.9.0;
 
 contract CrowdFunding {
     
+    struct Request {
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint numOfVotes;
+        mapping(address => bool) voters;
+    }
+    
+    
+    
     address public admin;
     mapping(address => uint) public contributors;
     uint public numOfContributors;
@@ -11,6 +22,8 @@ contract CrowdFunding {
     uint public deadline;
     uint public goal;
     uint public raisedAmount;
+    mapping(uint => Request) requests;
+    uint public numRequests;
     
     constructor(uint _goal, uint _deadline) {
         goal = _goal;
@@ -20,6 +33,7 @@ contract CrowdFunding {
     }
     
     function contribute() public payable {
+        
         require(block.timestamp < deadline);
         require(msg.value >= minimumContribution, "Minimum Contribution not met.");
         
@@ -33,6 +47,35 @@ contract CrowdFunding {
     
     function getBalance() public view returns(uint) {
         return address(this).balance;
+    }
+    
+    function getRefund() public {
+        
+        require(block.timestamp > deadline && raisedAmount < goal);
+        require(contributors[msg.sender] > 0);
+        
+        address payable recipient = payable(msg.sender);
+        uint value = contributors[msg.sender];
+        recipient.transfer(value);
+        
+        contributors[msg.sender] = 0;
+    }
+    
+    function createRequest(string memory _description, address payable _recipient, uint _value) public onlyAdmin {
+        
+        Request storage newRequest = requests[numRequests];
+        numRequests++;
+        
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.numOfVotes = 0;
+    }
+    
+    modifier onlyAdmin() {
+        require(msg.sender == admin);
+        _;
     }
     
     receive() payable external {
