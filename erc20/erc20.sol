c78ddf1d42e43a6d81fcb2931e8ab35627753ced
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2021-08-25 23:40:16 +0100

    Define erc20 partial-compliance functions

--- erc20/erc20.sol
+++ erc20/erc20.sol
@@ -8,9 +8,9 @@ interface ERC20Interface {
     function balanceOf(address tokenOwner) external view returns (uint balance);
     function transfer(address to, uint tokens) external returns (bool success);
     
-    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
-    function approve(address spender, uint tokens) external returns (bool success);
-    function transferForm(address from, address to, uint tokens) external returns (bool success);
+    //function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    //function approve(address spender, uint tokens) external returns (bool success);
+    //function transferForm(address from, address to, uint tokens) external returns (bool success);
     
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
@@ -21,9 +21,30 @@ contract Crypto is ERC20Interface {
     string public name = "My First Crypto";
     string public symbol = "MFC";
     uint public decimals = 0; //Usually 18
-    uint public override totalSupply = 1000000;
+    uint public override totalSupply;
     
     address public founder;
     mapping(address => uint) public balances;
     
+    constructor(){
+        totalSupply = 1000000;
+        founder = msg.sender;
+        balances[founder] = totalSupply;
+    }
+    
+    function balanceOf(address tokenOwner) public view override returns (uint balance) {
+        return balances[tokenOwner];
+    }
+    
+    function transfer(address to, uint tokens) public override returns (bool success) {
+        
+        require(balances[msg.sender] >= tokens);
+        
+        balances[to] += tokens;
+        balances[msg.sender] -= tokens;
+        
+        emit Transfer(msg.sender, to, tokens);
+        
+        return true;
+    }
 }
