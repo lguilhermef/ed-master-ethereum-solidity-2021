d08a96ced1c1640ba4452e1fbd1b9f7d177c3009
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2021-08-25 23:17:02 +0100

    Add erc20 interface

---
+++ erc20/erc20.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: GPL-3.int208
+
+pragma solidity >=0.5.0 <0.9.0;
+
+interface ERC20Interface {
+    
+    function totalSupply() external view returns (uint);
+    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function transfer(address to, uint tokens) external returns (bool success);
+    
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferForm(address from, address to, uint tokens) external returns (bool success);
+    
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
