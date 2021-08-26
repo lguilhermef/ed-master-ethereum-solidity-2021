af052ba0de98f6456d56cec03b4196584707852a
Luis Ferreira <luis.guilherme.ferreira@hotmail.com>
2021-08-26 12:37:08 +0100

    Add ERC20 full compliance

--- erc20/erc20.sol
+++ erc20/erc20.sol
@@ -8,9 +8,9 @@ interface ERC20Interface {
     function balanceOf(address tokenOwner) external view returns (uint balance);
     function transfer(address to, uint tokens) external returns (bool success);
     
-    //function allowance(address tokenOwner, address spender) external view returns (uint remaining);
-    //function approve(address spender, uint tokens) external returns (bool success);
-    //function transferForm(address from, address to, uint tokens) external returns (bool success);
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferForm(address from, address to, uint tokens) external returns (bool success);
     
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
@@ -25,6 +25,8 @@ contract Crypto is ERC20Interface {
     
     address public founder;
     mapping(address => uint) public balances;
+    
+    mapping(address => mapping(address => uint)) allowed;
     
     constructor(){
         totalSupply = 1000000;
@@ -44,6 +46,31 @@ contract Crypto is ERC20Interface {
         balances[msg.sender] -= tokens;
         
         emit Transfer(msg.sender, to, tokens);
+        
+        return true;
+    }
+    
+    function allowance(address tokenOwner, address spender) view public override returns(uint){
+        return allowed[tokenOwner][spender];
+    }
+    
+    function approve(address spender, uint tokens) public override return (bool success) {
+        require(balances[msg.sender] >= tokens);
+        require(tokens > 0);
+        
+        allowed[msg.sender][spender] = tokens;
+    
+        emit Approval(msg.spender, spender, tokens);
+        return true;
+    }
+    
+    function transferForm(address from, address to, uint tokens) public override returns (bool success) {
+        require(allowed[from][to] >= tokens);
+        require(balances[from] >= tokens);
+        
+        balances[from] -= tokens;
+        balances[to] += tokens;
+        allowed[from][to] -= tokens;
         
         return true;
     }
